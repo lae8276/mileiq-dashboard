145d50854d527fea1fc9b3644a6bc0b399da80fb
lae8276 <51336048+lae8276@users.noreply.github.com>
2025-08-02 21:39:36 +0100

    Update mileiq_dashboard.py

--- mileiq_dashboard.py
+++ mileiq_dashboard.py
@@ -50,6 +50,8 @@ def process_file(file) -> tuple[pd.DataFrame, float]:
     
     # Format date as dd-MMM-yyyy
     grouped['Date'] = pd.to_datetime(grouped['Date']).dt.strftime('%d-%b-%Y')
+grouped['Miles'] = grouped['Miles'].round(1)
+
 
     total_miles = grouped['Miles'].sum()
     return grouped, total_miles
